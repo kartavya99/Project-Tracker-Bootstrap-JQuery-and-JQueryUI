ab572042964993c4ebbbbf26ba7a2685575fa1e2
kartavya <kartavyas@gmail.com>
2023-04-02 12:24:55 +1000

    updated functionality to add project Name, Type, HourlRate and Duedate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,45 @@
 // save reference to  important DOM elements
 var timeDisplayEl = $("#time-display");
 var dueDateInputEl = $("#due-date-input");
+var projectNameInputEl = $("#project-name-input");
+var projectTypeInputEl = $("#project-type-input");
+var hourlyRateInputEl = $("#hourly-rate-input");
 
 // handle displaying the time
 function displayTime() {
   var rightNow = moment().format("MMM Dd, YYY [at] hh:mm:ss a");
   //   console.log(rightNow);
   timeDisplayEl.text(rightNow);
+}
+
+//handle printing project date to the page
+function printProjectData(name, type, hourlyRate, dueDate) {
+  // creating row element for the project
+  var projectRowEl = $("<tr>");
+
+  var projectNameTdEl = $("<td>").addClass("p-2").text(name);
+
+  var projectTypeTdEl = $("<td>").addClass("p-2").text(type);
+
+  var rateTdEl = $("<td>").addClass("p-2").text(hourlyRate);
+
+  var dueDateTdEl = $("<td>").addClass("p-2").text(dueDate);
+
+  // By listing each `<td>` variable as an argument, each one will be appended in that order
+  projectRowEl.append(projectNameTdEl, projectTypeTdEl, rateTdEl);
+}
+
+//handle project form submission
+function handleProjectFormSubmit(e) {
+  e.preventDefault();
+
+  var projectName = projectNameInputEl.val().trim();
+  var projectType = projectTypeInputEl.val().trim();
+  var hourlyRate = hourlyRateInputEl.val().trim();
+  var dueDate = dueDateInputEl.val().trim();
+
+  console.log(projectName, projectType, hourlyRate, dueDate);
+  printProjectData(projectName, projectType, hourlyRate, dueDate);
 }
 
 dueDateInputEl.datepicker({ mindDate: 1 });
