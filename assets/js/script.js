e4a28056065aca6bfad41473475f25365245b6a6
kartavya <kartavyas@gmail.com>
2023-04-03 13:09:43 +1000

    debut the error in html and more functionality in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,17 +28,46 @@ function printProjectData(name, type, hourlyRate, dueDate) {
 
   var dueDateTdEl = $("<td>").addClass("p-2").text(dueDate);
 
+  var daysLeftTdEl = $("<td>").addClass("p-2").text(dueDate);
+
+  var daysToDate = moment(dueDate, "MM/DD/YYYY").diff(moment(), "days");
+  var daysLeftTdEl = $("<td>").addClass("p-2").text(daysToDate);
+
+  var totalEarnings = calculateTOtalEarnings(hourlyRate, daysToDate);
+  //   console.log(totalEarnings);
+
+  var totalTdEl = $("<td>")
+    .addClass("p-2")
+    .text("$" + totalEarnings);
+  console.log(totalTdEl);
+
   // By listing each `<td>` variable as an argument, each one will be appended in that order
-  projectRowEl.append(projectNameTdEl, projectTypeTdEl, rateTdEl, dueDateTdEl);
+  projectRowEl.append(
+    projectNameTdEl,
+    projectTypeTdEl,
+    rateTdEl,
+    dueDateTdEl,
+    daysLeftTdEl,
+    totalTdEl
+  );
 
   projectDisplayEl.append(projectRowEl);
 
   projectModalEl.modal("hide");
 }
 
+//function to calculate total Earnings
+function calculateTOtalEarnings(rate, days) {
+  var dailyTotal = rate * 8;
+  //   console.log(dailyTotal);
+  var total = dailyTotal * days;
+  //   console.log(total);
+  return total;
+}
+
 //handle project form submission
-function handleProjectFormSubmit(e) {
-  e.preventDefault();
+function handleProjectFormSubmit(event) {
+  event.preventDefault();
 
   var projectName = projectNameInputEl.val().trim();
   var projectType = projectTypeInputEl.val().trim();
@@ -48,6 +77,8 @@ function handleProjectFormSubmit(e) {
   console.log(projectName, projectType, hourlyRate, dueDate);
 
   printProjectData(projectName, projectType, hourlyRate, dueDate);
+
+  projectFormEl[0].reset();
 }
 
 projectFormEl.on("submit", handleProjectFormSubmit);
